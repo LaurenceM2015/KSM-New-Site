2e0184e881d74f8dde07d46aa37febea92fd7c05
LaurenceM2015 <laurencemalonga@hotmail.com>
2019-05-17 05:41:41 +0100

    Add a custom field for the main header.

--- karate-shotokan-mardie-wp/page-home.php
+++ karate-shotokan-mardie-wp/page-home.php
@@ -17,36 +17,33 @@
      Template Name: Home Page
 */
 
+$course_url				= get_post_meta( 11, 'course_url', true );
+$button_text			= get_post_meta( 11, 'button_text', true );
+
 get_header();
 ?>
-    <!-- main Header -->
+      <!-- main Header -->
     <header class="header text-white" id="paralaxBkgimgJS">
-      <div class="header__jumbotron jumbotron-fluid">
-          <div class="container header__container h-100">
-              <div class="row h-100 align-items-center justify-content-center">
-                <div class="col-md-7">
-                  
-                  <div class="header__text hero-text animated fadeInLeft">
-                      <h1 class="text-left">Karate Shotokan Mardie</h1>
-                      <div class="">
-                        <p class="lead my-5">Les valeurs du club se trouvent dans l’effort, le bien être et la convivialité. N’hésitez pas à nous rejoindre</p>
-                        <a class="btn btn--orange" href="#slogan">En Savoir Plus</a>
-                      </div>
-                      
+        <div class="header__jumbotron jumbotron-fluid">
+            <div class="container header__container h-100">
+                <div class="row h-100 align-items-center justify-content-center">
+                  <div class="col-md-7">
+                    <div class="header__text hero-text animated fadeInLeft">
+                        <h1 class="text-left"><?php bloginfo('name'); ?></h1>
+                        <div class="">
+                          <p class="lead my-5"><?php bloginfo('description'); ?></p>
+                          <a class="js-scroll-trigger btn btn--orange" href="#<?php echo $course_url; ?>"><?php echo $button_text; ?></a>
+                        </div>
+                    </div>
                   </div>
-                  
-                </div>
-                <div class="col-md-5">
-                  <div class="header__img animated fadeInUp text-right">
-                  <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/shin.jpg" alt="KSM shine">
-                </div>
+                  <div class="col-md-5">
+                    <div class="header__img animated fadeInUp text-right">
+                    <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/shin.jpg" alt="KSM shine">
+                  </div>
+                  </div>
                 </div>
               </div>
-            </div>
-      </div>
-
-     
-      
+        </div>
     </header>
 
      <!-- Slogan with primary color -->
