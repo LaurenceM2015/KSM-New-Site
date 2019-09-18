81cf63f66ba252e34b05750575d9b2e84838a773
LaurenceM2015 <laurencemalonga@hotmail.com>
2019-09-18 20:43:18 +0100

    Change Info feature and about feature with WP  advance custom field.

--- karate-shotokan-mardie-wp/content-about.php
+++ karate-shotokan-mardie-wp/content-about.php
@@ -1,38 +1,41 @@
- 
- 
- <!-- apropo de nous -->
- <section id="apropodenous" class="page-section projects-section bg-light">
-    <div class="container">
-        <div class="row">
-          <div class="col-md-12 pb-5">
-            <h2 class="mb-5 heading-secondary heading-secondary--1">A Propos De Nous</h2>
-          </div>
+<?php
+
+// ABOUT THE KARATE CLUB
+
+   $about_us_heading      = get_field('about_us_heading');
+   $about_us_image        = get_field('about_us_image');
+   $about_us_subheading   = get_field('about_us_subheading');
+   $about_us_description  = get_field('about_us_description');
+
+?>
+
+<!-- apropo de nous -->
+<section id="apropodenous" class="page-section projects-section bg-light">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 pb-5">
+        <h2 class="mb-5 heading-secondary heading-secondary--1"><?php echo $about_us_heading; ?></h2>
+      </div>
+    </div>
+
+    <div class="row align-items-center no-gutters mb-4 mb-lg-5">
+      <div class="col-xl-5 col-lg-7">
+        <div class="text-center wow fadeInLeft">
+          <img class="img-fluid mb-3 mb-lg-0" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/profs3.jpg" alt="Les Professeurs de KSM, Valerie Mabilleau et Fabien Poulin">
+          <!-- If user uploaded image -->
+        <?php if( !empty($about_us_image)	) : ?>
+          <img src="<?php echo $about_us_image['url']; ?>"alt="<?php echo $about_us_image['alt']; ?>">
+        <?php endif; ?>
+      </div>
+    </div>
+      <div class="col-xl-7 col-lg-5 wow fadeInRight">
+        <div class="featured-text text-center text-lg-left">
+          <h3><?php echo $about_us_subheading; ?></h3>
+          <p class="text-black-50 ">
+          <?php echo $about_us_description; ?>
+          </p>
         </div>
-
-          <div class="row align-items-center no-gutters mb-4 mb-lg-5">
-            <div class="col-xl-5 col-lg-7">
-                <div class="text-center wow fadeInLeft">
-                  <img class="img-fluid mb-3 mb-lg-0" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/profs3.jpg" alt="Les Professeurs de KSM, Valerie Mabilleau et Fabien Poulin">
-                </div>
-               
-            </div>
-            <div class="col-xl-7 col-lg-5 wow fadeInRight">
-                <div class="featured-text text-center text-lg-left">
-                  <h3>Depuis Le Debut</h3>
-                  <p class="text-black-50 ">
-                      Le club de karaté de Mardié (KSM) à été crée en 1998 et compte aujourd'hui quelques 50 licenciés 
-                      dont une trentaine d'enfants (pour qui les cours sont départagés selon qu'ils sont gradés ou débutants).
-                  </p>
-
-                  <p class="text-black-50">
-                      C'est un club de proximité qui permet aux adultes et aux parents d'enfants des communes environnantes (Bou, Donnery, Fay aux Loges…) de ne pas être obligé de se déplacer plus loin. 
-                      Tout au long de l'année scolaire les licenciés peuvent avoir accès à de nombreux stages.
-                  </p>
-
-                 
-                </div>
-            </div>
-        </div>
-    </div><!-- container -->
-
+      </div>
+    </div>
+  </div><!-- container -->
 </section><!-- a propos de nous -->
--- karate-shotokan-mardie-wp/content-info-feature.php
+++ karate-shotokan-mardie-wp/content-info-feature.php
@@ -2,6 +2,9 @@
     // ADVANCED CUSTOMER FIELD
     $le_titre_des_info         = get_field('le_titre_des_info');
     $la_description_des_info   = get_field('la_description_des_info');
+
+    $button_url				= get_post_meta( 11, 'course_url', true );
+	$button_text			= get_post_meta( 11, 'button_text', true );
     
 
 ?>
@@ -16,30 +19,29 @@
           </div>
         </div>
   
-        <div class="row">
-
-        <?php $loop = new WP_Query( array( 'post_type' => 'info_feature', 'orderby' => 'post_id', 'order' => 'ASC') ); ?>
-
-        <?php while( $loop->have_posts() ) : $loop->the_post(); ?>
-            
-          <div class="col-lg-4 my-4">
-            <div class="feature-box card h-100">
-              <div class="card-img-top feature-box__img-box">
-              <img class="img-fluid feature-box__img" src="<?php if( has_post_thumbnail() ){ the_post_thumbnail(); } ?>">
-              </div>
-              <div class="card-body">
-                  <h4 class="card-title feature-box__text"><?php the_title(); ?></h4>
-                  <p class="card-text"><?php the_content(); ?></p>
-                      <a href="https://www.larep.fr/mardie-45430/actualites/un-stage-etait-organise-pour-aider-le-jeune-garcon_13121702/#refresh" target="_blank" class="btn-text">Lise l'article &rarr;</a>
-                </div>
-            </div>
-          </div> <!-- feature box 1 -->
-  
-          <?php endwhile; wp_reset_query(); ?>
-
-        </div><!-- row -->
-  
-  </div> <!-- container -->   
         
+    <div class="row">
 
+      <?php $loop = new WP_Query( array( 'post_type' => 'info_feature', 'orderby' => 'post_id', 'order' => 'ASC') ); ?>
+
+      <?php while( $loop->have_posts() ) : $loop->the_post(); ?>
+            
+        <div class="col-lg-4 my-4">
+          <div class="feature-box card h-100">
+            <div class="card-img-top feature-box__img-box">
+              <img class="img-fluid feature-box__img" src="<?php if( has_post_thumbnail() ){ the_post_thumbnail_url(); } ?>">
+            </div>
+            <div class="card-body">
+              <h4 class="card-title feature-box__text"><?php the_title(); ?></h4>
+              <p class="card-text"><?php the_content(); ?></p>
+              <a role="button" href="<?php echo $button_url; ?>" target="_blank" class="btn-text"><?php echo $button_text; ?> &rarr;</a>
+            </div>
+          </div>
+        </div> <!-- feature box 1 -->
+  
+      <?php endwhile; wp_reset_query(); ?>
+
+    </div><!-- row -->
+  </div> <!-- container -->   
 </section>
+
